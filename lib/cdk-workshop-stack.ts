99ef07592522bb095a46082a6d5f59322e9769ed
Antonio Masotti <toniomasotti@gmail.com>
2022-06-06 08:03:50 +0200

    add api gateway in front of the Lambda handler

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,16 +1,23 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-  const hello = new lambda.Function(this, 'HelloLambdaHandler', {
-    runtime: lambda.Runtime.NODEJS_16_X, // runtime environment
-    code: lambda.Code.fromAsset('lambda'), // code loaded from the lambda directory in this repo
-    handler: 'hello.handler' // look into a file called hello.js, take the function called 'handler'
-  })
+    // Lambda function declaration
+    const hello = new lambda.Function(this, 'HelloLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X, // runtime environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from the lambda directory in this repo
+      handler: 'hello.handler' // look into a file called hello.js, take the function called 'handler'
+    })
+
+    // API GATEWAY to make the lambda function accessible from the public internet
+    new apigw.LambdaRestApi(this, 'HelloEndpoint', {
+      handler: hello,
+    })
 
   }
 }
