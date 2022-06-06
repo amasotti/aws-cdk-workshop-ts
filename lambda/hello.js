928960119a724d397157025bfce5aa0c83d53566
Antonio Masotti <toniomasotti@gmail.com>
2022-06-06 08:16:01 +0200

    play around with available options in the lambda handler

--- lambda/hello.js
+++ lambda/hello.js
@@ -3,6 +3,6 @@ exports.handler = async function (event) {
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain"},
-        body:`Hello Dude and Good morning! You have hit ${event.path}\n`
+        body:`Hello Dude and Good morning! You have hit ${event.path}. Your method was ${event.httpMethod}\n`
     };
 }
